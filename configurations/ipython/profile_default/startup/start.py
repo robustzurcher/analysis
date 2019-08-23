374717b5b082fad76337f38f895dd12e43f2c10d
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2019-08-23 11:23:16 +0200

    workflow edits, draft of figure

---
+++ configurations/ipython/profile_default/startup/start.py
@@ -0,0 +1,14 @@
+from IPython import get_ipython
+ipython = get_ipython()
+
+ipython.magic('matplotlib inline')
+ipython.magic('load_ext autoreload')
+ipython.magic('autoreload 2')
+
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+DIR_FIGURES = os.environ['DIR_FIGURES']
---
+++ python/execute_notebooks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""This module executes all notebooks. It serves the main purpose to ensure that all can be
+executed and work proper independently."""
+import subprocess as sp
+import glob
+import os
+
+os.chdir(os.environ['PROJECT_ROOT'] + '/notebooks')
+
+for notebook in sorted(glob.glob('*.ipynb')):
+    
+    # TODO: The others are not yet ready for this.
+    if notebook != '00_illustrations.ipynb':
+        continue
+    
+    cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
+    sp.check_call(cmd, shell=True)
+
