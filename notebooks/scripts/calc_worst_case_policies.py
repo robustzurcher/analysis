f8d1019bbbebcb71a2330065c3c2a37b7e67c4da
MaxBlesch <s6mables@uni-bonn.de>
2019-08-31 12:14:48 +0200

    Rebased on add_robupy_calcs.

--- notebooks/scripts/calc_worst_case_policies.py
+++ notebooks/scripts/calc_worst_case_policies.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import numpy as np
 from worst_case_probs import create_worst_trans_mat
 from ruspy.estimation.estimation_cost_parameters import create_transition_matrix, \
@@ -10,3 +11,14 @@ costs = cost_func(num_states, lin_cost, params)
 
 trans_mat = create_transition_matrix(num_states, p_ml)
 worst_trans = create_worst_trans_mat(trans_mat, costs[0, 0:3], 0)
+=======
+from worst_case_probs import create_worst_trans_mat
+from ruspy.estimation.estimation_cost_parameters import create_transition_matrix
+
+num_states = 200
+
+
+
+trans_mat = create_transition_matrix()
+create_worst_trans_mat()
+>>>>>>> Rebased on add_robupy_calcs.
