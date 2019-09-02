6a21a633498de75563236726ec6099c3e26f8e38
MaxBlesch <s6mables@uni-bonn.de>
2019-09-02 14:04:48 +0200

    Rebasing.

---
+++ notebooks/scripts/calc_worst_case_policies.py
@@ -0,0 +1,9 @@
+from worst_case_probs import create_worst_trans_mat
+from ruspy.estimation.estimation_cost_parameters import create_transition_matrix
+
+num_states = 200
+
+
+
+trans_mat = create_transition_matrix()
+create_worst_trans_mat()
