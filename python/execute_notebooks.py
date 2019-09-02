f389b7ebe48951cc0972c40a592a2079f9953a63
MaxBlesch <s6mables@uni-bonn.de>
2019-09-02 13:15:11 +0200

    Final adjustments.

--- python/execute_notebooks.py
+++ python/execute_notebooks.py
@@ -8,10 +8,6 @@ import os
 os.chdir(os.environ['PROJECT_ROOT'] + '/notebooks')
 
 for notebook in sorted(glob.glob('*.ipynb')):
-    
-    # TODO: The others are not yet ready for this.
-    if notebook not in ['00_illustrations.ipynb', 'ivestigation.ipynb']:
-        continue
 
     
     cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
