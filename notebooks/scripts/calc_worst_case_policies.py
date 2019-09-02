dd9498c7fcbe3a1c02cb86e7e52207e44ecdcafd
MaxBlesch <s6mables@uni-bonn.de>
2019-09-02 14:14:59 +0200

    Finished rebasing.

--- notebooks/scripts/calc_worst_case_policies.py
+++ notebooks/scripts/calc_worst_case_policies.py
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import numpy as np
 from worst_case_probs import calc_fixp_worst
 from ruspy.estimation.estimation_cost_parameters import (
     cost_func,
     lin_cost,
+create_transition_matrix
 )
 import multiprocessing as mp
 import pickle
@@ -19,33 +19,8 @@ p_ml = np.loadtxt("resources/rust_trans_probs")
 params = np.loadtxt("resources/rust_cost_params")
 costs = cost_func(num_states, lin_cost, params)
 
-<<<<<<< HEAD
-trans_mat = create_transition_matrix(num_states, p_ml)
-worst_trans = create_worst_trans_mat(trans_mat, costs[0, 0:3], 0)
-=======
-=======
-import numpy as np
->>>>>>> First lines.
-from worst_case_probs import create_worst_trans_mat
-from ruspy.estimation.estimation_cost_parameters import create_transition_matrix, \
-    cost_func, lin_cost
-
-num_states = 200
-p_ml = np.loadtxt("resources/rust_trans_probs")
-params = np.loadtxt("resources/rust_cost_params")
-costs = cost_func(num_states, lin_cost, params)
-
-<<<<<<< HEAD
-
-
 trans_mat = create_transition_matrix()
-create_worst_trans_mat()
->>>>>>> Rebased on add_robupy_calcs.
-=======
-trans_mat = create_transition_matrix(num_states, p_ml)
-worst_trans = create_worst_trans_mat(trans_mat, costs[0, 0:3], 0)
->>>>>>> First lines.
-=======
+
 
 def wrapper_func(omega):
     rho = chi2.ppf(omega, len(p_ml) - 1) / (2 * (78 / 4292))
@@ -58,4 +33,3 @@ test = pool.map(
 )
 with open("results.pkl", "wb") as pkl_out:
     pickle.dump(test, pkl_out)
->>>>>>> Added script.
