e0d3164d0588b772d154c99d3b946b7af2eb5a7d
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2019-09-04 16:01:54 +0200

    worst case notebook just takes too long!

--- python/execute_notebooks.py
+++ python/execute_notebooks.py
@@ -11,7 +11,10 @@ for notebook in sorted(glob.glob('*.ipynb')):
     
     if 'sandbox' in notebook:
         continue
-    
+   
+    if 'worst' in notebook:
+        continue
+
     cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
     sp.check_call(cmd, shell=True)
 
