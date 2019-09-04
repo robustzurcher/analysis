2f14245e62ffbdf92ae3e4bce0142fcd71de8de2
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2019-09-04 07:53:13 +0200

    script improvements

--- notebooks/scripts/calc_worst_case_policies.py
+++ notebooks/scripts/calc_worst_case_policies.py
@@ -1,37 +1,60 @@
-import numpy as np
-from worst_case_probs import calc_fixp_worst
-from ruspy.estimation.estimation_cost_parameters import cost_func, lin_cost
+#!/usr/bin/env python
+""" TODO: please add proper module description."""
+from functools import partial
 import multiprocessing as mp
-import pickle
-from scipy.stats import chi2
-import os
+import pickle as pkl
+import shutil
 import glob
+import os
 
-workers = 30
-omega_range = np.linspace(0, 1, 26)
-# omega_range = np.linspace(0, 1, 3)  # Debugging mode
+from ruspy.estimation.estimation_cost_parameters import cost_func
+from ruspy.estimation.estimation_cost_parameters import lin_cost
+from scipy.stats import chi2
+import numpy as np
+
+from worst_case_probs import calc_fixp_worst
 
 
-def wrapper_func(omega):
-    beta = 0.9999
-    # num_states = 25  # Debugging
-    num_states = 400
+NUM_WORKERS = 3
+NUM_POINTS = 20
+NUM_STATES = 200
+BETA = 0.9999
+
+# For debugging purposes, just set to a very large number. Then the there will only be a single
+# fix point iteration.
+THRESHOLD = 1e-7
+
+
+def wrapper_func(beta, num_states, threshold, omega):
+
     p_ml = np.loadtxt("resources/rust_trans_probs.txt")
     params = np.loadtxt("resources/rust_cost_params.txt")
+
     costs = cost_func(num_states, lin_cost, params)
     rho = chi2.ppf(omega, len(p_ml) - 1) / (2 * (4292 / 78))
-    result = calc_fixp_worst(num_states, p_ml, costs, beta, rho, threshold=1e-7)
-    pickle.dump(
-        result,
-        open("results/intermediate_{}.pkl".format(np.around(omega, decimals=2)), "wb"),
-    )
+
+    result = calc_fixp_worst(num_states, p_ml, costs, beta, rho, threshold)
+
+    # TODO: Please avoid linebreaks for readability and use proper formatting capabilities.
+    fname = "results/intermediate_{}.pkl".format('{:3.5f}'.format(omega))
+    pkl.dump(result, open(fname, "wb"))
+
     return result
 
 
 if __name__ == "__main__":
-    os.makedirs("results", exist_ok=True)
-    pool = mp.Pool(workers)
-    final_result = pool.map(wrapper_func, omega_range)
-    pickle.dump(final_result, open("results/final_result.pkl", "wb"))
-    for file in glob.glob("results/intermediate*"):
-        os.remove(file)
+
+    grid_omega = np.linspace(0, 1, NUM_POINTS)
+
+    # TODO: We want to make sure all results from a previous run are deleted and not then
+    #  processed further as well down the line.
+    if os.path.exists("results"):
+        shutil.rmtree('results')
+    os.mkdir('results')
+
+    # TODO: This allows to include the constant arguments in the MP function.
+    p_wrapper_func = partial(wrapper_func, BETA, NUM_STATES, THRESHOLD)
+    final_result = mp.Pool(NUM_WORKERS).map(p_wrapper_func, grid_omega)
+
+    pkl.dump(final_result, open("results/final_result.pkl", "wb"))
+    [os.remove(file) for file in glob.glob("results/intermediate*")]
