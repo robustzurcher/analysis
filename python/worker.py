b191c23e24360b97849068103b8ac7d41aeb7fca
MaxBlesch <s6mables@uni-bonn.de>
2019-09-09 14:34:24 +0200

    Final version.

---
+++ python/worker.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""This script provides all capabilities for the worker processes."""
+import os
+
+# In this script we only have explicit use of MPI as our level of parallelism. This needs to be
+# done right at the beginning of the script.
+update = {'NUMBA_NUM_THREADS': '1', 'OMP_NUM_THREADS': '1', 'OPENBLAS_NUM_THREADS': '1',
+          'NUMEXPR_NUM_THREADS': '1', 'MKL_NUM_THREADS': '1'}
+os.environ.update(update)
+
+import pickle as pkl
+import numpy as np
+import json
+
+from scipy.stats import chi2
+from mpi4py import MPI
+
+from ruspy.estimation.estimation_cost_parameters import cost_func
+from ruspy.estimation.estimation_cost_parameters import lin_cost
+
+from worst_case_probs import calc_fixp_worst
+
+
+def wrapper_func(p_ml, costs, beta, num_states, threshold, omega):
+    rho = chi2.ppf(omega, len(p_ml) - 1) / (2 * (4292 / 78))
+    result = calc_fixp_worst(num_states, p_ml, costs, beta, rho, threshold)
+    fname = "results/intermediate_{}.pkl".format('{:3.5f}'.format(omega))
+    pkl.dump(result, open(fname, "wb"))
+
+    return result
+
+
+p_rust = np.loadtxt("resources/rust_trans_probs.txt")
+params_rust = np.loadtxt("resources/rust_cost_params.txt")
+spec = json.load(open('specification.json', 'rb'))
+
+comm = MPI.Comm.Get_parent()
+
+# We want to let the master know we are ready to go
+costs_rust = cost_func(spec['num_states'], lin_cost, params_rust)
+base_args = (p_rust, costs_rust, spec['beta'], spec['num_states'], spec['threshold'])
+
+while True:
+
+    comm.Send([np.zeros(1, dtype='float'), MPI.DOUBLE], dest=0)
+
+    cmd = np.array(0, dtype='int64')
+    comm.Recv([cmd, MPI.INT], source=0)
+
+    if cmd == 0:
+        comm.Disconnect()
+        break
+
+    if cmd == 1:
+        omega = np.zeros(1, dtype='float')
+        comm.Recv([omega, MPI.DOUBLE], source=0)
+        wrapper_func(*base_args, omega[0])
