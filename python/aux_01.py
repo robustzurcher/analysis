80bb3e2db6f909742afa8e3bcb20cbdf514637bb
MaxBlesch <s6mables@uni-bonn.de>
2019-09-16 11:10:50 +0200

    Added plots.

--- python/aux_01.py
+++ python/aux_01.py
@@ -6,6 +6,7 @@ def create_sections(mean_disc, om_range):
     state_sections = []
     for j, i in enumerate(np.unique(mean_disc)):
         where = mean_disc == i
+        max_ind = np.max(np.where(mean_disc == i))
         if j == 0:
             med_val = (np.max(om_range[where]) + np.min(om_range[~where])) / 2
             omega_sections += [np.append(om_range[where], med_val)]
@@ -16,7 +17,7 @@ def create_sections(mean_disc, om_range):
             state_sections += [np.array([i] + mean_disc[where].tolist())]
         else:
             low = (np.min(om_range[where]) + np.max(omega_sections[-1][:-1])) / 2
-            high = (np.max(om_range[where]) + np.min(om_range[mean_disc == i + 1])) / 2
+            high = (np.max(om_range[where]) + np.min(om_range[max_ind + 1])) / 2
             omega_sections += [np.array([low] + om_range[where].tolist() + [high])]
             state_sections += [np.array([i] + mean_disc[where].tolist() + [i])]
     return omega_sections, state_sections
