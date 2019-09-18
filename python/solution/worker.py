d53137fa95e021e276a247ffe4b0effe8ed066f9
MaxBlesch <s6mables@uni-bonn.de>
2019-09-18 16:56:10 +0200

    Finished fixpoint scripts.

--- python/solution/worker.py
+++ python/solution/worker.py
@@ -21,12 +21,9 @@ from ruspy.estimation.estimation_cost_parameters import lin_cost
 from worst_case_policies import calc_fixp_worst
 
 
-def wrapper_func(p_ml, obs_per_state, costs, beta, num_states, threshold, omega):
-    if num_states > obs_per_state.shape[0]:
-        raise AssertionError
-    inv_omega_state = chi2.ppf(np.full(obs_per_state.shape[0], omega), len(p_ml) - 1)
-    rho_state = np.divide(inv_omega_state, 2 * obs_per_state)
-    result = calc_fixp_worst(num_states, p_ml, costs, beta, rho_state, threshold)
+def wrapper_func(p_ml, costs, beta, num_states, threshold, omega):
+    rho = chi2.ppf(omega, len(p_ml) - 1) / 2 * (4292 / 388)
+    result = calc_fixp_worst(num_states, p_ml, costs, beta, rho, threshold)
     fname = "results/intermediate_{}.pkl".format('{:3.5f}'.format(omega))
     pkl.dump(result, open(fname, "wb"))
 
@@ -35,15 +32,13 @@ def wrapper_func(p_ml, obs_per_state, costs, beta, num_states, threshold, omega)
 
 p_rust = np.loadtxt("../../pre_processed_data/parameters/p_1000_4.txt")
 params_rust = np.array([10, 10])
-obs_state = np.loadtxt("../../pre_processed_data/parameters/obs_state_1000.txt")
 spec = json.load(open('specification.json', 'rb'))
 
 comm = MPI.Comm.Get_parent()
 
 # We want to let the master know we are ready to go
 costs_rust = cost_func(spec['num_states'], lin_cost, params_rust)
-base_args = (p_rust, obs_state, costs_rust, spec['beta'], spec['num_states'],
-             spec['threshold'])
+base_args = (p_rust, costs_rust, spec['beta'], spec['num_states'], spec['threshold'])
 
 while True:
 
