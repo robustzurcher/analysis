a575b9d91ac253b624a12c9d46ff8a7fe9973920
MaxBlesch <s6mables@uni-bonn.de>
2019-09-28 20:50:33 +0200

    Introduced file deletion.

--- python/validation/worker.py
+++ python/validation/worker.py
@@ -49,14 +49,16 @@ while True:
         dict_polcies = get_file(spec["policy_dict"])
         fixp_rob = dict_polcies[fixp_key][0]
         fixp_ml = dict_polcies[0.0][0]
-        
+
         np.random.seed()
         trans = create_asym_trans_mat(fixp_rob.shape[0], raw_params, raw_hesse_inv)
 
         df_rob = simulate(spec, fixp_rob, trans)
         performance_rob = discount_utility(df_rob, spec["periods"], spec["beta"])[-1]
+        del df_rob
 
         df_ml = simulate(spec, fixp_ml, trans)
         performance_ml = discount_utility(df_ml, spec["periods"], spec["beta"])[-1]
+        del df_ml
 
         pkl.dump((performance_ml, performance_rob), open(fname, "wb"))
